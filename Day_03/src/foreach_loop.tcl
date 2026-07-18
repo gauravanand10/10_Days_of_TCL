9f5ba47a2c61d907318a31145695993ddfe12c92
gauravanand10 <ganand_be23@thapar.edu>
2026-07-18 17:00:05 +0000

    Create foreach_loop.tcl

---
+++ Day_03/src/foreach_loop.tcl
@@ -0,0 +1,13 @@
+#=========================================================
+# Day 3 : foreach Loop
+# Author : Gaurav
+#=========================================================
+
+
+puts "\nforeach Loop"
+
+set fruits {Apple Mango Orange Banana}
+
+foreach fruit $fruits {
+    puts $fruit
+}
