9100bb7ca989b2ab2f3be464b5a050bb2f511c0f
gauravanand10 <ganand_be23@thapar.edu>
2026-07-25 13:28:59 +0000

    Create basic_procedure.tcl

---
+++ Day_05/src/basic_procedure.tcl
@@ -0,0 +1,18 @@
+#=========================================================
+# Day 5 : Basic Procedures
+# Author : Gaurav
+#=========================================================
+
+puts "\nBasic Procedure"
+
+proc greet {} {
+    puts "Hello, World!"
+}
+
+greet
+
+proc introduction {} {
+    puts "My name is Gaurav"
+}
+
+introduction
