00952396be393de4bb23a72df30698b1bcf7c7ad
gauravanand10 <ganand_be23@thapar.edu>
2026-08-06 10:20:21 +0000

    Create array_names.tcl

---
+++ Day_08/src/array_names.tcl
@@ -0,0 +1,16 @@
+#=========================================================
+# Day 8 : Array Names
+# Author : Gaurav
+#=========================================================
+
+# Create an array
+
+array set marks {
+    Rahul 85
+    Amit 90
+    Neha 76
+}
+
+# Display all array indices
+
+puts [array names marks]
