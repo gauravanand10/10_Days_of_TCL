73044b4fdd08c45edb7348dd6c28dd9f33a3070e
gauravanand10 <ganand_be23@thapar.edu>
2026-08-06 10:19:59 +0000

    Create array_get.tcl

---
+++ Day_08/src/array_get.tcl
@@ -0,0 +1,16 @@
+#=========================================================
+# Day 8 : Array Get
+# Author : Gaurav
+#=========================================================
+
+# Create an array
+
+array set marks {
+    Rahul 85
+    Amit 90
+    Neha 76
+}
+
+# Display the complete array
+
+puts [array get marks]
