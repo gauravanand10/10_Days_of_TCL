b5ca3ff86ebbf045b667b8275e45ca82cababd84
gauravanand10 <ganand_be23@thapar.edu>
2026-08-06 10:19:39 +0000

    Create array_exists.tcl

---
+++ Day_08/src/array_exists.tcl
@@ -0,0 +1,16 @@
+#=========================================================
+# Day 8 : Array Exists
+# Author : Gaurav
+#=========================================================
+
+# Create an array
+
+array set marks {
+    Rahul 85
+    Amit 90
+    Neha 76
+}
+
+# Check if the array exists
+
+puts [array exists marks]
