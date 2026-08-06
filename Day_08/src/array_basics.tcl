3ec319ee8df751bdfa8cec36d57cde1e7d36207b
gauravanand10 <ganand_be23@thapar.edu>
2026-08-06 10:19:19 +0000

    Create array_basics.tcl for array demonstration
    Added a Tcl script demonstrating basic array usage.

---
+++ Day_08/src/array_basics.tcl
@@ -0,0 +1,16 @@
+#=========================================================
+# Day 8 : Arrays Basics
+# Author : Gaurav
+#=========================================================
+
+# Create an array
+
+array set marks {
+    Rahul 85
+    Amit 90
+    Neha 76
+}
+
+puts "Rahul : $marks(Rahul)"
+puts "Amit  : $marks(Amit)"
+puts "Neha  : $marks(Neha)"
